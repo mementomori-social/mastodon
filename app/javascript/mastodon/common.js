acf894f7676b293a4db6e0eb7c9f76fecba6a5bb
Roni Laukkarinen <roni@dude.fi>
2023-07-09 22:23:59 +0300

    Hide top bar when scrolling down

--- app/javascript/mastodon/common.js
+++ app/javascript/mastodon/common.js
@@ -10,3 +10,50 @@ export function start() {
     // If called twice
   }
 }
+
+// Hide the top bar when scrolling down, show it when scrolling up
+let lastScrollTop = 0;
+let lastScrollDirection = 0;
+let lastScrollTime = 0;
+let scrollTimeout = null;
+
+function scrollHandler() {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const scrollDirection = scrollTop > lastScrollTop ? 1 : -1;
+  const scrollTime = Date.now();
+
+  if (scrollDirection !== lastScrollDirection) {
+    lastScrollDirection = scrollDirection;
+    lastScrollTime = scrollTime;
+  }
+
+  if (scrollTimeout) {
+    clearTimeout(scrollTimeout);
+  }
+
+  if (scrollDirection === 1) {
+    document.body.classList.remove('scroll-up');
+    document.body.classList.add('scroll-down');
+  } else {
+    document.body.classList.remove('scroll-down');
+    document.body.classList.add('scroll-up');
+
+    if (scrollTime - lastScrollTime > 1000) {
+      document.body.classList.add('scroll-top');
+    }
+  }
+
+  scrollTimeout = setTimeout(() => {
+    document.body.classList.remove('scroll-top');
+  }
+, 1000);
+
+  lastScrollTop = scrollTop;
+}
+
+export function enableScrollHandler() {
+  window.addEventListener('scroll', scrollHandler);
+}
+
+// Launch
+enableScrollHandler();
