6bc5a50a57d1d8142b6a8d5bf5ba45850779be3a
Roni Laukkarinen <roni@dude.fi>
2023-07-12 12:34:20 +0300

    Improve hiding top bar on mobile

--- app/javascript/mastodon/common.js
+++ app/javascript/mastodon/common.js
@@ -21,6 +21,35 @@ function scrollHandler() {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   const scrollDirection = scrollTop > lastScrollTop ? 1 : -1;
   const scrollTime = Date.now();
+  const scrollPositionFromTop = window.scrollY;
+
+  // If not mobile, bail
+  if (window.innerWidth > 890) {
+    return;
+  }
+
+  // When the scroll position from top touches .notification__filter-bar, add class
+  const notificationFilterBar = document.querySelector('.notification__filter-bar');
+  const notificationFilterBarHeight = notificationFilterBar ? notificationFilterBar.offsetHeight : 0;
+  const tabsBarWrapperHeight = document.querySelector('.tabs-bar__wrapper') ? document.querySelector('.tabs-bar__wrapper').offsetHeight : 0;
+  const topBarHeight = notificationFilterBarHeight + tabsBarWrapperHeight;
+
+  if (notificationFilterBar) {
+    const notificationFilterBarRect = notificationFilterBar.getBoundingClientRect();
+    if (scrollPositionFromTop > 146) {
+      notificationFilterBar.classList.add('notification__filter-bar--fixed');
+    }
+
+    // When the fixed item reaches the amount of its height from the top, remove class
+    if (scrollPositionFromTop <= topBarHeight) {
+      notificationFilterBar.classList.remove('notification__filter-bar--fixed');
+    }
+  }
+
+  // If scroll position from bottom is less than a certain amount, don't do anything
+  if (scrollPositionFromTop < 1200) {
+    return;
+  }
 
   if (scrollDirection !== lastScrollDirection) {
     lastScrollDirection = scrollDirection;
@@ -38,7 +67,7 @@ function scrollHandler() {
     document.body.classList.remove('scroll-down');
     document.body.classList.add('scroll-up');
 
-    if (scrollTime - lastScrollTime > 1000) {
+    if (scrollTime - lastScrollTime > 100) {
       document.body.classList.add('scroll-top');
     }
   }
@@ -46,13 +75,26 @@ function scrollHandler() {
   scrollTimeout = setTimeout(() => {
     document.body.classList.remove('scroll-top');
   }
-, 1000);
+  , 100);
 
   lastScrollTop = scrollTop;
 }
 
 export function enableScrollHandler() {
   window.addEventListener('scroll', scrollHandler);
+
+  // Remove scroll-down class on click and touch events
+  window.addEventListener('click', () => {
+    scrollHandler();
+    document.body.classList.remove('scroll-down');
+  });
+
+  // Each time div .columns-area__panels__main has changed, call scrollHandler
+  const columnsAreaPanelsMain = document.querySelector('.columns-area__panels__main');
+  if (columnsAreaPanelsMain) {
+    const observer = new MutationObserver(scrollHandler);
+    observer.observe(columnsAreaPanelsMain, { childList: true });
+  }
 }
 
 // Launch
