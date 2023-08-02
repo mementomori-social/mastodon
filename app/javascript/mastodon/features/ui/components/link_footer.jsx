c245c2908d5b0d006dc81a61f14d3ad2c6260e65
Roni Laukkarinen <roni@dude.fi>
2023-08-02 10:12:59 +0300

    Update to Mastodon Bird UI 1.7.1rc2

--- app/javascript/mastodon/features/ui/components/link_footer.jsx
+++ app/javascript/mastodon/features/ui/components/link_footer.jsx
@@ -102,7 +102,8 @@ class LinkFooter extends PureComponent {
           {DividingCircle}
           <a href={source_url} rel='noopener noreferrer' target='_blank'><FormattedMessage id='footer.source_code' defaultMessage='View source code' /></a>
           {DividingCircle}
-          v{version}+nightly-2023-07-31~mementomori-social-mods + <a href='https://github.com/ronilaukkarinen/mastodon-bird-ui/tree/main'>Mastodon Bird UI 1.7.1rc1-nightly</a>
+          v{version}+nightly-2023-07-31~mementomori-social-mods + <a 
+href='https://github.com/ronilaukkarinen/mastodon-bird-ui/tree/main'>Mastodon Bird UI 1.7.1rc2-nightly</a>
         </p>
       </div>
     );
